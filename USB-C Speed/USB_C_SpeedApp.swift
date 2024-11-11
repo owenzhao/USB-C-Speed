b60ee011bc7c8ccedbfe43326a704060efbe292e
Zhao Xin <owenzx@gmail.com>
2024-11-11 11:31:30 +0800

    add menubar icon
    * remove ContentView.swift

--- USB-C Speed/USB_C_SpeedApp.swift
+++ USB-C Speed/USB_C_SpeedApp.swift
@@ -9,9 +9,49 @@ import SwiftUI
 
 @main
 struct USB_C_SpeedApp: App {
+  @State private var usbData: USBData = USBData(spusbDataType: [])
+  @State private var isMenuBarViewPresented = true
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      USBDataView(usbData: usbData)
+        .task {
+          let text = listUSBDevices()
+
+          let decoder = JSONDecoder()
+          let jsonData = Data(text.utf8)
+
+          do {
+            let usbData = try decoder.decode(USBData.self, from: jsonData)
+            self.usbData = usbData
+            print(usbData)
+          } catch {
+            print(error)
+          }
+        }
+    }
+
+    // 修改 MenuBarExtra 以使用自定义视图
+    MenuBarExtra("USB-C Speed", systemImage: "bolt.fill") {
+      SimplifiedUSBDataView(usbData: usbData)
+    }
+    .menuBarExtraStyle(.window)
+  }
+
+  // 通过system_profiler SPUSBDataType读取USB设备信息
+  func listUSBDevices() -> String {
+    let task = Process()
+    task.executableURL = URL(fileURLWithPath: "/usr/sbin/system_profiler")
+    task.arguments = ["SPUSBDataType", "-json"]
+    let pipe = Pipe()
+    task.standardOutput = pipe
+
+    do {
+      try task.run()
+      let data = pipe.fileHandleForReading.readDataToEndOfFile()
+      return String(data: data, encoding: .utf8) ?? ""
+    } catch {
+      return "Error: \(error.localizedDescription)"
     }
   }
 }
